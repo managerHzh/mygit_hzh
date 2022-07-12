87876f20b426d45cef1e9ab1a1fee1ff081fa269
hzh <1337627207@qq.com>
2022-07-12 22:16:58 +0800

    hzh程序员更新

---
+++ git_hzh_001/src/main/java/com/bjpowernode/pojo/Student.java
@@ -0,0 +1,7 @@
+package com.bjpowernode.pojo;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("我是一名学生!!！");
+    }
+}
---
+++ git_hzh_002/src/main/java/com/bjpowernode/controller/Newcode.java
@@ -0,0 +1,7 @@
+package com.bjpowernode.controller;
+
+public class Newcode {
+    public static void main(String[] args) {
+        System.out.println("我是新的类!!!");
+    }
+}
